eb21f398c3339e7c2eb69b1e2445f808618998a5
hyejin-Jo <konnimey@naver.com>
2025-12-03 18:40:09 +0900

    feat: 커서 커스텀 작업 완료 & 부드러운 스크롤 구현 (Lenis 적용)

--- src/app/_modules/common/layout/AppLayout.tsx
+++ src/app/_modules/common/layout/AppLayout.tsx
@@ -1,11 +1,52 @@
 'use client';
 
+import { useEffect } from 'react';
+import Lenis from '@studio-freight/lenis';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import Header from '../header/Header';
+import CustomCursor from '../cursor/CustomCursor';
+import { useIsMobile } from '../hooks/useIsMobile';
 import * as S from './styled';
 
 const AppLayout = ({ children }: { children: React.ReactNode }) => {
+  const { isMobile, isLoaded } = useIsMobile();
+
+  useEffect(() => {
+    if (!isLoaded || isMobile) return;
+
+    // Lenis 초기화
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      orientation: 'vertical',
+      gestureOrientation: 'vertical',
+      smoothWheel: true,
+      wheelMultiplier: 1,
+      touchMultiplier: 2,
+      infinite: false,
+    });
+
+    // GSAP ScrollTrigger와 Lenis 통합
+    lenis.on('scroll', ScrollTrigger.update);
+
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000);
+    });
+
+    gsap.ticker.lagSmoothing(0);
+
+    return () => {
+      lenis.destroy();
+      gsap.ticker.remove((time) => {
+        lenis.raf(time * 1000);
+      });
+    };
+  }, [isLoaded, isMobile]);
+
   return (
     <S.AppLayoutContainer>
+      <CustomCursor />
       <Header />
       {children}
     </S.AppLayoutContainer>
