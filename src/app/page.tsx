385e5af75a776cde9faaff5cd51dc5c72af223d4
hyejin-Jo <konnimey@naver.com>
2025-12-23 14:01:39 +0900

    fix: 검색 결과 노출 표시 내용 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next';
 import MainContainer from './_modules/main/main-container/MainContainer';
 
 export const metadata: Metadata = {
-  title: 'Home',
+  title: 'Hyejin Jo Portfolio',
   description:
-    '프론트엔드 개발자 조혜진의 포트폴리오 사이트입니다. React, Next.js, TypeScript를 활용한 웹 개발 경험, 프로젝트 및 스킬을 소개합니다.',
+    '프론트엔드 개발자 조혜진의 포트폴리오 사이트입니다. 저의 웹 개발 경험과 프로젝트를 소개합니다 :)',
 };
 
 export default function Home() {
